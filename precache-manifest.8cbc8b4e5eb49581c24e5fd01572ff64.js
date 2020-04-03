1b60ca13a902c8664287ecf99366af6bdfdc2648
StaceyZhan <styg51201@gmail.com>
2020-04-03 22:36:31 +0800

    Updates

---
+++ precache-manifest.8cbc8b4e5eb49581c24e5fd01572ff64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b6f38e58622ffbf9fdff9400bf1e8b",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "9518a54b9414752dc1a8",
+    "url": "/testapp/static/css/main.0fbcb44d.chunk.css"
+  },
+  {
+    "revision": "7214671e958fd858456b",
+    "url": "/testapp/static/js/2.fc61ad80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp/static/js/2.fc61ad80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9518a54b9414752dc1a8",
+    "url": "/testapp/static/js/main.8bd1c9f1.chunk.js"
+  },
+  {
+    "revision": "740e23f2138b7f6c06c0",
+    "url": "/testapp/static/js/runtime-main.2bcdff7b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/testapp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
